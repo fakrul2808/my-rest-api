7030219cf35cb80974c8f4fb35212b31b4778561
fakrul2808 <143020267+fakrul2808@users.noreply.github.com>
2023-12-12 03:44:22 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'Fahri Hosting'// yourname
 global.MONGO_DB_URI = "" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
+global.your_email = "fahristoree2808@gmail.com" //email
 global.email_password = "" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
